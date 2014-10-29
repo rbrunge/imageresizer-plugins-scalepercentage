2aa186933be6d71eed24a8113ab19505a7f7fa09
Robert Runge <robertboerunge@hotmail.com>
2014-10-29 12:42:45 +0100

    First Commit

---
+++ ScalePercentage/ScalePercentagePlugin.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using ImageResizer.Configuration;
+using ImageResizer.Resizing;
+
+namespace ImageResizer.Plugins.ScalePercentage
+{
+    public class ScalePercentagePlugin : BuilderExtension, IPlugin, IQuerystringPlugin
+    {
+        private const string PluginName = @"scalepercentage";
+
+        public IPlugin Install(Config c)
+        {
+            c.Plugins.add_plugin(this);
+            return this;
+        }
+
+        protected override RequestedAction PostDecodeStream(ref Bitmap b, ResizeSettings settings)
+        {
+            var scalePercentage = settings[PluginName];
+            if (string.IsNullOrEmpty(scalePercentage))
+            {
+                return RequestedAction.None;
+            }
+
+            int scaling = 0;
+            try
+            {
+                scaling = 100/int.Parse(scalePercentage);
+                if (scaling >= 1)
+                {
+                    return RequestedAction.None;
+                }
+            }
+            catch
+            {
+                return RequestedAction.None;
+            }
+            var settingsString = string.Format("width={0}", (int)(b.Width / scaling));
+            var stream = new MemoryStream();
+            ImageBuilder.Current.Build(b, stream, new ResizeSettings(settingsString));
+            b = new Bitmap(stream);
+            return RequestedAction.None;
+        }
+
+      
+
+        public bool Uninstall(Config c)
+        {
+            c.Plugins.remove_plugin(this);
+            return true;
+        }
+
+        public IEnumerable<string> GetSupportedQuerystringKeys()
+        {
+            return new string[] { PluginName };
+        }
+    }
+}
